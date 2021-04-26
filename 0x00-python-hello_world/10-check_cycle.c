67c4f845d5c52aea2a63dc84c7e464ad8e0496b9
dantereto <2247@holbertonschool.com>
2021-04-26 23:53:44 +0000

    check

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,14 +7,14 @@
 int check_cycle(listint_t *list)
 {
 listint_t *slow = list, *fast = list;
-if (list == '\0' && list->next == '\0')
+if (list == '\0' || list->next == '\0')
 return (0);
 while (slow != '\0' && fast != '\0' && fast != '\0')
 {
-if (slow == fast)
-return (1);
 slow = slow->next;
 fast = fast->next->next;
+if (slow == fast)
+return (1)
 }
 return (0);
 }
