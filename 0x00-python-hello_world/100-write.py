3c5ebe5ee7fa75e1492d7c1bc6387ae8403191ac
dantereto <2247@holbertonschool.com>
2021-04-26 21:00:03 +0000

    write with sys

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+var = sys.stdout
+arr = ["and that piece of art is useful - Dora Korpar, 2015-10-19\n"]
+for i in arr:
+    var.write(i)
 sys.exit(1)
