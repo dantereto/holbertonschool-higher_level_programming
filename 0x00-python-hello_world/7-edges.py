0066afa499417ca94531957c0ca1d66a2811fc4b
dantereto <2247@holbertonschool.com>
2021-04-26 17:41:59 +0000

    edges

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = str(word[:3])
-word_last_2 = str(word[7:9])
+word_last_2 = str(word[-2:])
 middle_word = str(word[1:-1])
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
