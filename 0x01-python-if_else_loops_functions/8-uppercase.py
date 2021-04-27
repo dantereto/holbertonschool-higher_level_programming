56ba40ee335abd49e7afea1a81c406eb317eb528
dantereto <2247@holbertonschool.com>
2021-04-27 19:40:15 +0000

    upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,5 +5,5 @@ def uppercase(str):
         cont = ord(i)
         if cont >= 97 and cont <= 122:
             cont -= 32
-        print(chr(cont), end='')
+        print("{:c}".format(cont), end='')
     print()
