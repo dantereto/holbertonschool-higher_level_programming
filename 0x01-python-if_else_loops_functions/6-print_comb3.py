03e3a62ea6c896be6891f1f8ce6242508bd9fbcd
dantereto <2247@holbertonschool.com>
2021-04-27 19:35:50 +0000

    print 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(9):
+for i in range(0, 9):
     for j in range(i + 1, 10):
-        print(str(i) + str(j), end=', ')
+        if i < 8:
+            print("{:d}{:d}".format(i, j), end=', ')
+        else:
+            print("{:d}{:d}".format(i, j))
