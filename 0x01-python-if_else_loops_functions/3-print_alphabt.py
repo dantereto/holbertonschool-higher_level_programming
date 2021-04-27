e8a214e0a17d1c26c326af9e8719262c8fa95596
dantereto <2247@holbertonschool.com>
2021-04-27 18:51:41 +0000

    alpha

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 123):
     if i == 101 or i == 113:
-        i += 1
+        continue
     else:
-        print('{}'.format(chr(i)), end='')
+        print('{:c}'.format(i), end='')
