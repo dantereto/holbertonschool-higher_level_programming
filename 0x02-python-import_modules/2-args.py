a94bd6183cbcb8859de7e9aa38521be8f67d4f92
dantereto <2247@holbertonschool.com>
2021-04-28 17:56:58 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,12 @@ import sys
 if __name__ == '__main__':
     suma = len(sys.argv)
     if suma == 1:
-        print('{:d} arguments.'.format(suma - 1))
+        print('{} arguments.'.format(suma - 1))
     elif suma == 2:
-        print('{:d} arguments:'.format(suma - 1))
+        print('{} arguments:'.format(suma - 1))
     else:
-        print('{:d} arguments:'.format(suma - 1))
+        print('{} arguments:'.format(suma - 1))
     for args in range(0, suma):
         if args > 0:
-            print("{:d}: {:s}"
+            print("{}: {}"
                   .format(args, sys.argv[args]))
