376bd41fe0afbed0d4f32e1157dd9f8b647817fc
dantereto <2247@holbertonschool.com>
2021-04-28 03:27:32 +0000

    insert a number

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,18 +2,24 @@
 #include <stdlib.h>
 
 /**
- * check_cycle -find a cycle
- * @list: the header
+ * insert_node - intsrt a node
+ * @head: the header
+ * @n: the number
  * Return: NULL.
  */
 listint_t *insert_node(listint_t **head, int n)
 {
-  listint_t *new;
-  new = malloc(sizeof(listint_t));
-  if (new == '\0')
-    return ('\0');
-  new->n = n;
-  new->next = *head;
-  *head = new;
-  return (new);
+listint_t *new, *head2 = *head;
+new = malloc(sizeof(listint_t));
+if (new == '\0')
+return ('\0');
+new->n = n;
+new->next = NULL;
+if (head == NULL)
+return ('\0');
+while (head2->next != '\0' && head2->next->n < new->n)
+head2 = head2->next;
+new->next = head2->next;
+head2->next = new;
+return (new);
 }
