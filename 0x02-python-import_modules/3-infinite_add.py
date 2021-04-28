96b342eb0a43702839d94c467b689ec6930d57b4
dantereto <2247@holbertonschool.com>
2021-04-28 15:26:44 +0000

    infinite sum

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys 
+if __name__ == '__main__':
+    suma = 0
+    cont = len(sys.argv)
+    for args in range(1, cont):
+        suma += int(sys.argv[args])
+    print("{:d}".format(suma))
