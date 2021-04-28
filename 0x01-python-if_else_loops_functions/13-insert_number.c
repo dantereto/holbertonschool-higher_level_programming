3b585d8728d51f08d64d48d7e300e10a9cf1467a
dantereto <2247@holbertonschool.com>
2021-04-28 00:46:26 +0000

    insert

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * check_cycle -find a cycle
+ * @list: the header
+ * Return: NULL.
+ */
+listint_t *insert_node(listint_t **head, int n)
+{
+  listint_t *new;
+  new = malloc(sizeof(listint_t));
+  if (new == '\0')
+    return ('\0');
+  new->n = n;
+  new->next->next = *head;
+  *head = new;
+  return (new);
+}
