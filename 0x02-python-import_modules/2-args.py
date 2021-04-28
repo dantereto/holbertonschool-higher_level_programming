c69ea93d9a752b41651eafdf518dd0042669d0cd
dantereto <2247@holbertonschool.com>
2021-04-28 17:14:22 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 import sys
 if __name__ == '__main__':
     suma = len(sys.argv)
-    if suma == 0:
+    if suma <= 1:
         print('0 arguments.')
     print('{:d} arguments:'.format(suma - 1))
     for args in range(1, suma):
