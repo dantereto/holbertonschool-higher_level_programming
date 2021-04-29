fec05cf4c87ba5b372f0cc6c1ec5e95e3d7d4601
dantereto <2247@holbertonschool.com>
2021-04-29 02:59:08 +0000

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    sep = '__'
     dirs = dir(hidden_4)
     for i in range(0, len(dirs)):
-        if sep not in dirs[i]:
-            print(dirs[num])
+        if '__' not in dirs[i]:
+            print(dirs[i])
