797bb474af78380837c4075c9801b38a63c5d2e4
dantereto <2247@holbertonschool.com>
2021-04-29 00:24:34 +0000

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import hidden_4
-    for num in dir(hidden_4):
-        if num[0:2] == "__":
+    for i in dir(hidden_4):
+        if i[0:2] == '__':
             continue
         print("{}".format(i))
