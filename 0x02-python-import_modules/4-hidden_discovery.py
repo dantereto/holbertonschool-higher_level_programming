da19645c0ac83d417d7dbc41da1d6df0f466b938
dantereto <70924466+dantereto@users.noreply.github.com>
2021-04-28 19:35:25 -0500

    Delete 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == '__main__':
-    import hidden_4
-    dirs = dir(hidden_4)
-    sep = "__"
-    for num in range(0, len(dirs)):
-        if '__' not in dirs[num]:
-            print(dirs[num])
