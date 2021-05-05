deba62e784b0caa92c5e2326dffba14b84223947
dantereto <2247@holbertonschool.com>
2021-05-05 20:21:15 +0000

    check the max integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if copy == None:
+        return
+    copy = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > copy:
+            copy = my_list[i]
+    return (copy)
