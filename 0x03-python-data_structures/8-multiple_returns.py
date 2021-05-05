c7ea693318880fd8c20455dcef4de9d4d70383d5
dantereto <2247@holbertonschool.com>
2021-05-05 19:59:52 +0000

    make two returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,4 +2,5 @@
 def multiple_returns(sentence):
     if len(sentence) < 0:
         return setence([0], None)
-    return(len(sentence), sentence[0])
+    else:
+        return(len(sentence), sentence[0])
