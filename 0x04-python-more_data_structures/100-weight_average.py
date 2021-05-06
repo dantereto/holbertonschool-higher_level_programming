d14913fd85a84a66cbcf407640a800993a6b0f9e
dantereto <2247@holbertonschool.com>
2021-05-06 21:47:11 +0000

    average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    val = 0
+    val2 = 0
+    for i in my_list:
+        val = val + (i[1])
+        val2 = val2 + (i[0] * i[1])
+    return(val2 / val)
