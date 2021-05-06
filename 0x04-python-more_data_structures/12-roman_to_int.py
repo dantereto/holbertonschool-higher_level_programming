6dedd8561009aa8533c678370ce59413d9951572
dantereto <2247@holbertonschool.com>
2021-05-06 21:03:10 +0000

    change to roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
     r_dic = {"I": 1, "V": 5, "X": 10, "L": 50
              , "C": 100, "D": 500, "M": 1000}
     for i in range(len(roman_string)):
-        if i == len(roman_string) - 1:  
+        if i == len(roman_string) - 1:
             sum += r_dic[roman_string[i]]
         elif r_dic[roman_string[i]] >= r_dic[roman_string[i + 1]]:
             sum += r_dic[roman_string[i]]
