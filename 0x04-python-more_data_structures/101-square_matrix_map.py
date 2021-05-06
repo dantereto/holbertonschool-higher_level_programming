042a25ec0c36e369d7c4280647c4b73319e768b4
dantereto <2247@holbertonschool.com>
2021-05-06 23:11:07 +0000

    double map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return(list(map(lambda sec: list(map(lambda num: num ** 2, sec)), matrix))) 
+    return(list(map(lambda sec: list(map(lambda num: num ** 2, sec)), matrix)))
