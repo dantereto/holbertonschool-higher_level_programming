80ad61befda176876c2e24d2eea8576c8e44a04c
dantereto <2247@holbertonschool.com>
2021-05-06 18:19:49 +0000

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     maxi = None
     if a_dictionary:
-        maxi = max(a_dictionary, key = a_dictionary.get)
+        maxi = max(a_dictionary, key=a_dictionary.get)
         return (maxi)
     else:
         return
