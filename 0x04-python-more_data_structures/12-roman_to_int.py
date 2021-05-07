c8344dd4b6d9fcc450642c8a0e64f3dbe533cf77
dantereto <2247@holbertonschool.com>
2021-05-07 00:35:43 +0000

    betty

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,8 @@ def roman_to_int(roman_string):
     if roman_string == None or type(roman_string) != str:
         return (0)
     sum = 0
-    r_dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    r_dic = {"I": 1, "V": 5, "X": 10, "L": 50,
+             "C": 100, "D": 500, "M": 1000}
     for i in range(len(roman_string)):
         if i == len(roman_string) - 1:
             sum += r_dic[roman_string[i]]
