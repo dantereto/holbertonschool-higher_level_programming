020cba80cb311ce7c429ba2c31df81030d3bebee
dantereto <2247@holbertonschool.com>
2021-05-17 16:56:46 +0000

    super safe

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+    except ValueError as err:
+        print("Exception: {}".format(err), file = sys.stderr)
+        return(False)
+    return(True)
+    
