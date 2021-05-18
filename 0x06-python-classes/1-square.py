d3c645f0805974ba5d0ad770dba033140b24e956
dantereto <2247@holbertonschool.com>
2021-05-18 23:40:06 +0000

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """class square"""
+
+
 class Square:
-"""initialize"""
-     def __init__(self, size):
-         self.__size = size
+    """initialize"""
+
+    def __init__(self, size):
+        self.__size = size
