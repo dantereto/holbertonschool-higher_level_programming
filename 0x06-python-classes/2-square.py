0cb1dd9391aa383bf80e7778d339eadbb869f981
dantereto <2247@holbertonschool.com>
2021-05-18 23:44:47 +0000

    square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """class square"""
+
+
 class Square:
      """start"""
+
+
      def __init__(self, size=0):
           if type(size) is not int:
                raise TypeError('size must be an integer')
