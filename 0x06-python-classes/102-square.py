39e649f1d27507c502bd539ede1b50f7ba9a800c
dantereto <2247@holbertonschool.com>
2021-05-18 21:38:33 +0000

    it work

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 class Square:
+     def __init__(self, size=0):
+         self.size = size
      @property
      def size(self):
          return(self.__size)
@@ -12,15 +14,15 @@ class Square:
           self.__size = value
      def area(self):
           return(self.__size ** 2)
-def __eq__(self, other):
-     return self.size == self.other
-def __ne__(self, other):
-     return self.size != self.other
-def __gr__(self, other):
-     return self.size > self.other
-def __ge__(self, other):
-     return self.size >= self.other
-def __lt__(self, other):
-     return self.size < self.other
-def __le__(self, other):
-     return self.size <= self.other
+     def __eq__(self, other):
+          return self.size == other.size
+     def __ne__(self, other):
+          return self.size != other.size
+     def __gr__(self, other):
+          return self.size > other.size
+     def __ge__(self, other):
+          return self.size >= other.size
+     def __lt__(self, other):
+          return self.size < other.size
+     def __le__(self, other):
+          return self.size <= other.size
