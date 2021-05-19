264da2be524b33f66d63e4561438fceb04e2f147
dantereto <2247@holbertonschool.com>
2021-05-19 00:25:27 +0000

    square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """class square"""
+
+
 class Square:
-    """initialize"""
+    """int: Module level variable documented inline.
+
+The docstring may span multiple lines. The type may optionally be specified
+on the first line, separated by a colon.
+    """
     def __init__(self, size=0):
           if type(size) is not int:
                raise TypeError('size must be an integer')
