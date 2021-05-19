962367dc56ecabc36d84eecec75a3643be8f9d84
dantereto <2247@holbertonschool.com>
2021-05-19 00:07:14 +0000

    square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,5 +8,6 @@ class Square:
           if size < 0:
                raise ValueError('size must be >= 0')
           self.__size = size
+     """get the area"""
      def area(self):
           return(self.__size ** 2)
