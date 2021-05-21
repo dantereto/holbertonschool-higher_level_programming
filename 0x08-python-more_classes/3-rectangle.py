150e93e140bc70e0fda4f630d8d630e7200bd8ef
dantereto <2247@holbertonschool.com>
2021-05-21 00:56:41 +0000

    Rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""create the class"""
+
+
 class Rectangle:
     """dont do anything"""
     def __init__(self, width=0, height=0):
-        self.__width = width 
+        self.__width = width
         self.__height = height
+
     @property
     def width(self):
         return(self.__width)
+
     @width.setter
     def width(self, value):
         if type(value) != int:
@@ -14,9 +19,11 @@ class Rectangle:
         if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
+
     @property
     def height(self):
         return(self.__height)
+
     @height.setter
     def height(self, value):
         if type(value) != int:
@@ -24,13 +31,15 @@ class Rectangle:
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
+
     def area(self):
         return (self.__width * self.__height)
+
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return(0)
         return (self.__width * 2) + (self.__height * 2)
-    
+
     def __str__(self):
         size = '#' * self.__width
         copy = size
