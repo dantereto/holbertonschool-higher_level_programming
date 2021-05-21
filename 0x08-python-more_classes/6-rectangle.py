7a6d8b5314c23f0c3bbd56f05d55fc5124a7a0b2
dantereto <2247@holbertonschool.com>
2021-05-21 01:21:35 +0000

    rectangle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
+"""create the class"""
+
+
 class Rectangle:
     number_of_instances = 0
     """dont do anything"""
+
     def __init__(self, width=0, height=0):
-        self.__width = width 
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
+
     @property
     def width(self):
         return(self.__width)
+
     @width.setter
     def width(self, value):
         if type(value) != int:
@@ -16,9 +22,11 @@ class Rectangle:
         if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
+
     @property
     def height(self):
         return(self.__height)
+
     @height.setter
     def height(self, value):
         if type(value) != int:
@@ -26,21 +34,25 @@ class Rectangle:
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
+
     def area(self):
         return (self.__width * self.__height)
+
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return(0)
         return (self.__width * 2) + (self.__height * 2)
-    
+
     def __str__(self):
         size = '#' * self.__width
         copy = size
         for i in range(self.height):
             copy += '\n' + size
         return copy
+
     def __repr__(self):
         return('Rectangle({:d}, {:d})'.format(self.__width, self.__height))
+
     def __del__(self):
         print('Bye rectangle...')
         Rectangle.number_of_instances -= 1
