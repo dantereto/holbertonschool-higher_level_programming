0096e97cef7c6c8d86cec141e949fbe1942088d4
dantereto <2247@holbertonschool.com>
2021-05-25 01:16:51 +0000

    text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    if type(text) != str:
+        raise TypeError('text must be a string')
+    for ch in range (len(text)):
+        if ch > 0 and text[ch] == ' ' and text[ch - 1] in '?.:':
+            continue
+        print(text[ch], end='')
+        if text[ch] == '?' or text[ch] == '.' or text[ch] == ':':
+            print('\n')
