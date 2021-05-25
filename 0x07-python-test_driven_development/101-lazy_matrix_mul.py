619ed07505f008d7fcb9abbc6c49fd722df3a283
dantereto <2247@holbertonschool.com>
2021-05-25 11:55:55 -0500

    lazy

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -3,7 +3,3 @@ import numpy as np
 def lazy_matrix_mul(m_a, m_b):
     res =  np.matmul(m_a, m_b)
     return (res)
-#!/usr/bin/python3
-
-print(lazy_matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
-print(lazy_matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
