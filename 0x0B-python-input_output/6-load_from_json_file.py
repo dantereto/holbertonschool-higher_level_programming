0e8660f2859f89bbb6a62e90cfac265d2d5f11b9
dantereto <2247@holbertonschool.com>
2021-05-31 23:26:04 +0000

    pep

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""create function"""
+
+
 import json
+
+
 def load_from_json_file(filename):
+    """ creates an Object from a “JSON file”"""
     with open(filename) as f:
         return json.load(f)
