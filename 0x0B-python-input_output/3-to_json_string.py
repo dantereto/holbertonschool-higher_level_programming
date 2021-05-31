96499ca13b88b2a189fb34cbcdbf8e8199f06c44
dantereto <2247@holbertonschool.com>
2021-05-31 23:21:35 +0000

    pep

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""create funcion"""
+
+
 import json
+
+
 def to_json_string(my_obj):
-     return json.dumps(my_obj)
+    """return the json"""
+
+    return json.dumps(my_obj)
