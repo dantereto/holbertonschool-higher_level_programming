5f8cc638b582fe1d3add91756937bf31b739f8a4
dantereto <2247@holbertonschool.com>
2021-05-31 16:18:37 +0000

    add elements in a list

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+try:
+    list_js = load_from_json_file('add_item.json')
+except:
+    list_js = []
+finally:
+    for argc in sys.argv[1:]:
+        list_js.append(argc)
+    save_to_json_file(list_js, 'add_item.json')
