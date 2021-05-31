93a9573657fa118cee4f53d009a40919daa0670d
dantereto <2247@holbertonschool.com>
2021-05-31 23:17:31 +0000

    pep

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""create function"""
+
+
 def append_write(filename="", text=""):
-    with open(filename, 'a', encoding="utf-8") as f:
+    """appends a string at the end of a text file"""
+
+    with open(filename, 'a', encoding="UTF-8") as f:
         return f.write(text)
