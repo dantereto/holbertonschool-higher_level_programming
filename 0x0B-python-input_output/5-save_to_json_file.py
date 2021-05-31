1a31e67b29710a67d49a36a0315139db98035ea6
dantereto <2247@holbertonschool.com>
2021-05-31 23:24:39 +0000

    pep

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""create function"""
+
+
 import json
+
+
 def save_to_json_file(my_obj, filename):
+    """ writes an Object to a text file"""
+
     with open(filename, 'w', encoding="utf-8") as f:
         json.dump(my_obj, f)
