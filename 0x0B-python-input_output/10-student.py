9c5bdfb8025d25a72285b5a58cc56af5d0d7699d
dantereto <2247@holbertonschool.com>
2021-05-31 20:49:44 +0000

    student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self, attrs=None):
+        
+        _dict = {}
+        if type(attrs) == list:
+            for i in attrs:
+                if type(i) == int:
+                    _dict = self.__dict__ 
+                try:
+                    _dict[i] = getattr(self, i)
+                except:
+                    pass
+        else:
+            _dict = self.__dict__
+        return (_dict)
