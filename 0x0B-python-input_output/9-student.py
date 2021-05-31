58d1f542b1c8f3f504492a14e8daabf9a7058a5f
dantereto <2247@holbertonschool.com>
2021-05-31 23:32:57 +0000

    pep

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""create the class"""
+
 
 class Student:
+    """Instantiation"""
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
+    """retrieves a dictionary representation of a Student"""
     def to_json(self):
         return (self.__dict__)
