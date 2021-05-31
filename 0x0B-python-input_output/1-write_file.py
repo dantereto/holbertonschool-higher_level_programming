83b30a498295d75c7fb7b7dfe26e21a2f497e41b
dantereto <2247@holbertonschool.com>
2021-05-31 23:15:31 +0000

    pep

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""creste function"""
+
+
 def write_file(filename="", text=""):
-     with open(filename, 'w', encoding="utf-8") as f:
+    """ writes a string to a text file """
+
+    with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
