a2ca02a8236ac1b6962506d3596ce5e5890da471
dantereto <2247@holbertonschool.com>
2021-06-01 02:28:15 +0000

    pascal

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,5 +11,6 @@ def pascal_triangle(n):
         pascals_triangle.append([1] * (i + 1))
     for i in range(2, n):
         for j in range(1, i):
-            pascals_triangle[i][j] = pascals_triangle[i-1][j-1] + pascals_triangle[i - 1][j]
+            pascals_triangle[i][j] = pascals_triangle[i-1][j-1] \
+                                     + pascals_triangle[i - 1][j]
     return pascals_triangle
