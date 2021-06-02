ee8df77f45565167fb78a9a57bcd40263a11d266
dantereto <2247@holbertonschool.com>
2021-06-02 19:11:18 +0000

    super square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""create the class"""
+
+
+class BaseGeometry:
+    """check the area"""
+
+    def area(self):
+        raise Exception('area() is not implemented')
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
+    def __str__(self):
+        string = ('[Rectangle] ' + str(self.__width) + '/' + str(self.__height))
+        return string
+    def area(self):
+        return (self.__width * self.__height)
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        super().__init__(size, size)
+        self.__size = size
+    def __str__(self):
+        string = ('[Square] ' + str(self.__size) + '/' + str(self.__size))
+        return (string)
