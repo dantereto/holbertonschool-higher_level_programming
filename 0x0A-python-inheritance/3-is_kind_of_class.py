f8ae44a323f7664611507340ae15941799cdc4bc
dantereto <2247@holbertonschool.com>
2021-06-03 00:35:47 +0000

    pep

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""create the function"""
+
+
 def is_kind_of_class(obj, a_class):
+    """returns if the object is an instance or not"""
+
     if isinstance(obj, a_class):
         return True
     else:
