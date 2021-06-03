d5f1c605bdd28140d79a7781bc052f446f00316f
dantereto <2247@holbertonschool.com>
2021-06-03 01:21:14 +0000

    pep

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -7,21 +7,28 @@ class BaseGeometry:
 
     def area(self):
         raise Exception('area() is not implemented')
+
     def integer_validator(self, name, value):
         if type(value) != int:
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
+"""create a class"""
+
 
 class Rectangle(BaseGeometry):
+    """start"""
+
     def __init__(self, width, height):
         self.integer_validator('width', width)
         self.__width = width
         self.integer_validator('height', height)
         self.__height = height
+
     def __str__(self):
-        string = ('[Rectangle] ' + str(self.__width) + '/' + str(self.__height))
+        string = '[Rectangle] ' + str(self.__width) + '/'
+        string += str(self.__height)
         return string
+
     def area(self):
         return (self.__width * self.__height)
-
