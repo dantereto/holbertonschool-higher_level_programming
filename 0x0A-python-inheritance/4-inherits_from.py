12b614f0f094997eb86b9a241a2d41ce9dfb4d7b
dantereto <2247@holbertonschool.com>
2021-06-03 00:40:20 +0000

    pep

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""create the function"""
+
+
 def inherits_from(obj, a_class):
+    """check the inherit class"""
+
     if type(obj) != a_class:
         return issubclass(type(obj), a_class)
     else:
