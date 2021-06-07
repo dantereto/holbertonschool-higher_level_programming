451a387ca6a6cc8dfa5d9f791808d62b2790e32e
dantereto <2247@holbertonschool.com>
2021-06-07 00:36:10 +0000

    base and json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,52 @@
 #!/usr/bin/python3
+"""start the function"""
+
+
 import json
+
+
+import os
+
+
 class Base:
+    """start"""
+
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects 
+            self.id = Base.__nb_objects
+
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or list_dictionaries is 0:
             return ('[]')
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = (cls.__name__) + '.json'
+        list_o = []
+        if list_objs is not None:
+            for i in list_objs:
+                list_o.append(i.to_dictionary())
+        with open(filename, 'w') as f:
+            return f.write(Base.to_json_string(list_o))
+
+    def from_json_string(json_string):
+        if json_string is None or json_string is 0:
+            return ('[]')
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == 'Rectangle':
+            from models.rectangle import Rectangle
+            dummy = Rectangle(1, 1)
+        if cls.__name__ == 'Square':
+            from models.square import Square
+            dummy = Square(1, 1)
+        dummy.update(**dictionary)
+        return (dummy)
