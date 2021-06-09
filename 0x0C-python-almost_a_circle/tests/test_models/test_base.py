1420fa7345e03726736bd9b48034daf45e3e95e0
dantereto <2247@holbertonschool.com>
2021-06-09 01:14:06 +0000

    cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,5 +9,17 @@ class TestMaxInteger(unittest.TestCase):
     def test_cases(self):
         b1 = Base()
         self.assertEqual(1, b1.id)
+        b1 = Base(34)
+        self.assertEqual(34, b1.id)
+        b1 = Base(-1)
+        self.assertEqual(-1, b1.id)
+        b1 = Base('hola')
+        self.assertEqual('hola', b1.id)
+        b1 = Base({'id': 48, 'size': 12})
+        self.assertEqual({'id': 48, 'size': 12}, b1.id)
+        b1 = Base([1, 2, 3])
+        self.assertEqual([1, 2, 3], b1.id)
+        b1 = Base([1, 2, -3])
+        self.assertEqual([1, 2, -3], b1.id)
 if __name__ == '__main__':
     unittest.main()
