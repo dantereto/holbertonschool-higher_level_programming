fb13ceb9a1ce8330350025b7507efdfe58038378
dantereto <2247@holbertonschool.com>
2021-07-13 20:57:26 +0000

    cities by state

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- all the cities
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id = cities.id
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id;
