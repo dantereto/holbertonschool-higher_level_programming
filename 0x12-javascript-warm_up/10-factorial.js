467d2496bec124114ae9544ebd2d140384c89953
dantereto <2247@holbertonschool.com>
2021-08-02 18:21:54 -0500

    check

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,8 @@ const x = parseInt(process.argv[2]);
 function factorial (x) {
   if (x === 0) {
     return 1;
+  } else {
+    return (x * factorial(x - 1));
   }
-  return (x * factorial(x - 1));
 }
 console.log(factorial(x));
