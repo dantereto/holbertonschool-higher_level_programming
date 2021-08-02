1c1ba30c02bf289226044481f654abbb9f8f25e5
dantereto <2247@holbertonschool.com>
2021-08-02 18:43:37 -0500

    recursion error

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
 const num = parseInt(process.argv[2]);
 function factorial (x) {
-  if (x === 0) {
+  if (isNaN(x)) {
+  console.log(1);
+}
+  if (x === 1) {
     return 1;
   }
   return x * factorial(x - 1);
+}
+if (isNaN(num)) {
+  console.log(1);
 }
 if (num > 0) {
   console.log(factorial(num));
