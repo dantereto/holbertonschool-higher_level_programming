42f2fa006e13ef5b8aa2af5d10fbaa919f924902
dantereto <2247@holbertonschool.com>
2021-08-02 10:16:47 -0500

    make a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2]))
+{
+console.log("Not a number");
+}
+else{
+for (let i = 0; i < process.argv[2]; i++) {
+        console.log("X".repeat(process.argv[2]));
+}
+}
