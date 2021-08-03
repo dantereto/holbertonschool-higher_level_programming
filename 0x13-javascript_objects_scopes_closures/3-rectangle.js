22c234652582f372521243480f01c09404360702
dantereto <2247@holbertonschool.com>
2021-08-03 08:33:08 -0500

    print the square

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (h, w) {
+    if (w > 0 && h > 0) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.width; i++) {
+      console.log('X'.repeat(this.height));
+    }
+  }
+};
