37ef6b7720e4e4768a3f6022b3fe3c2397f9b8fc
dantereto <2247@holbertonschool.com>
2021-08-06 02:18:33 +0000

    error solved

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,11 @@ if __name__ == '__main__':
         db=sys.argv[3])
 
     cursor = db.cursor()
-    cursor.execute("SELECT c.id, c.name, s.name FROM cities AS c JOIN states AS s ON c.state_id  = s.id ORDER BY c.id")
+    cursor.execute("SELECT c.id, c.name, s.name\
+    FROM cities AS c\
+    JOIN states AS s\
+    ON c.state_id  = s.id\
+    ORDER BY c.id")
     data = cursor.fetchall()
     for row in data:
         print(row)
