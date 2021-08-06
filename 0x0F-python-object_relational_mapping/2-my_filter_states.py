6c22de8d2a44ad2075873d10f180ecb869736fce
dantereto <2247@holbertonschool.com>
2021-08-06 00:16:21 +0000

    filter

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    data = cursor.fetchall()
+    for row in data:
+        if row[1] == sys.argv[4]:
+            print(row)
+    cursor.close()
+    db.close()
