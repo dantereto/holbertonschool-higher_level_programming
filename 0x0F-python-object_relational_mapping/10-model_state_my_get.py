c7c27bde94435b044f289ee9949b3ecd270957bd
dantereto <2247@holbertonschool.com>
2021-08-06 17:09:12 +0000

    get the argv element

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                       .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                       pool_pre_ping=True)
+
+if __name__ == '__main__':
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first = session.query(State).filter(State.name == sys.argv[4]).first()
+    if first is None:
+        print("Not found")
+    else:
+        print("{}".format(first.id))
+    session.close()
