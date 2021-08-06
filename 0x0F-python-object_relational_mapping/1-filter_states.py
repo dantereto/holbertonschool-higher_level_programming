ad852a600a1834fcea86add2b55d71b30fde228d
dantereto <2247@holbertonschool.com>
2021-08-06 00:04:07 +0000

    search the N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd="root",
+        db=sys.argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name like 'N%' ")
+    data = cursor.fetchall()
+    for row in data:
+        print(row)
+    cursor.close()
+    db.close()
