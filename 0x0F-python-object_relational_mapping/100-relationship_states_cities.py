3805475eba2389b54487c5dee809f3aa987a2ccb
dantereto <2247@holbertonschool.com>
2021-08-07 04:03:51 +0000

    try failed

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from relationship_city import City
+from relationship_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                       .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                       pool_pre_ping=True)
+
+if __name__ == '__main__':
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    newS = State(name = 'California')
+    newC = City(name = 'San Francisco')
+    newS.cities.append(newC)
+    session.add(newS)
+    session.commit()
+    session.close()
