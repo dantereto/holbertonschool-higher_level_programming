be3248616b7709ce2a7c1efe0ec9892f213b149a
dantereto <2247@holbertonschool.com>
2021-08-07 21:21:34 +0000

    check

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3])
-
+    
     cursor = db.cursor()
     cursor.execute("SELECT c.name\
     FROM cities AS c\
