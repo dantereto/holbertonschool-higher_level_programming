b9285b3a5ebee5cc4925e2d6acd83b74e3a764a5
dantereto <2247@holbertonschool.com>
2021-08-07 22:47:58 +0000

    check

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3])
-    
+
     cursor = db.cursor()
     cursor.execute("SELECT c.name\
     FROM cities AS c\
@@ -18,10 +18,9 @@ if __name__ == '__main__':
     ON c.state_id  = s.id\
     WHERE s.name = %s\
     ORDER BY c.id", (sys.argv[4],))
-    data = cursor.fetchall()
-    for row in data[0:-1]:
-        print(row[0], end=', ')
-    print(data[0][-1], end="")
-    print("")
+    cities = []
+    for data in cursor.fetchall():
+        cities.append(data[0])
+    print(', '.join(cities))
     cursor.close()
     db.close()
