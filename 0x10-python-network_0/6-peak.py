caf7c630ce9503fd03ecc0f641dfa06b563cbf30
dantereto <2247@holbertonschool.com>
2021-08-20 01:05:49 +0000

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,7 +6,10 @@ def find_peak(list_of_integers):
     result = 0
     if not list_of_integers:
         return None
-    i = 1
-    list_of_integers.sort()
-    result = list_of_integers[-1]
+    i = 0
+    result = list_of_integers[1]
+    n = len(list_of_integers)
+    for i in range(1, n):
+        if list_of_integers[i] >= result:
+            result = list_of_integers[i]
     return result
