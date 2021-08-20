12e86cd13a4f224d14a62dc61b97ee0729e613c9
dantereto <2247@holbertonschool.com>
2021-08-20 18:49:42 +0000

    pep

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,6 @@
 """start the code"""
 import urllib.request
 
-
 if __name__ == '__main__':
     req = urllib.request.Request('https://intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
