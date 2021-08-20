490ac95c83a033fe957f1f5ca3b8434abd3499b0
dantereto <2247@holbertonschool.com>
2021-08-20 15:19:09 -0500

    print my personal github id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""start the code"""
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == '__main__':
+    r = requests.get('https://api.github.com/user', auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+    print(r.json().get('id'))
