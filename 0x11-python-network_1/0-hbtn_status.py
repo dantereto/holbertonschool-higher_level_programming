63d95c8fd85e614d58049714a8991e0da246dc98
dantereto <2247@holbertonschool.com>
2021-08-20 18:55:30 +0000

    check

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     req = urllib.request.Request('https://intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(the_page)))
-        print('\t- conetnt: {}'.format(the_page))
-        print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
+    print('Body response:')
+    print('\t- type: {}'.format(type(the_page)))
+    print('\t- conetnt: {}'.format(the_page))
+    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
